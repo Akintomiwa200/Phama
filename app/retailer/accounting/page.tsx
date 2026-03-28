b362248c349aa647df9355ecf2ada59cbeb8ff27
akintomiwa200 <herkintormiwer@gmail.com>
2026-03-28 02:01:02 +0100

    update and solve errors

--- app/retailer/accounting/page.tsx
+++ app/retailer/accounting/page.tsx
@@ -1,5 +1,5 @@
 import { DashboardLayout } from '@/components/layout/DashboardLayout';
-import { AccountingPage } from '../../../wholesaler/accounting/AccountingPage';
+import { AccountingPage } from '../../wholesaler/accounting/AccountingPage';
 export default function Page() {
   return <DashboardLayout portal="retailer"><AccountingPage /></DashboardLayout>;
 }
